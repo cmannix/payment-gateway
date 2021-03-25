afde253a89ec087296ff21f0ab0e04688ba43912
Chris Mannix <cmannix@hey.com>
2021-03-25 09:28:17 +0000

    Initial commit with API project creation

---
+++ src/PaymentGateway.Api/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PaymentGateway.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(string id) => NotFound($"Payment '{id}' not found");
+    }
+}
---
+++ src/PaymentGateway.Api/Payment.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace PaymentGateway.Api
+{
+    public record Payment(string Description);
+}
